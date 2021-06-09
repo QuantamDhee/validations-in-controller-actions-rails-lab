e9b713f40ba44b85bb3593cfa1c15be167cd5767
Johndhee <Johndheeballesteros13@gmail.com>
2021-06-09 16:04:12 -0700

    done with Chau Tran

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "/authors/new", to: "authors#new", as: "new_author"
   get "/authors/:id", to: "authors#show", as: "author"
   post "/authors", to: "authors#create"
+  resources :authors, only: :index
 
   get "/posts/:id", to: "posts#show", as: "post"
   get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
